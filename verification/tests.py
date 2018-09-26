5273e711b742bfe4af2b4916001c7ceb84df69a6
XanderVi <engineermexanic@gmail.com>
2018-09-26 10:56:35 +0300

    add ~17 new tests

--- verification/tests.py
+++ verification/tests.py
@@ -59,6 +59,101 @@ TESTS = {
         {
             "input":[5, [2, 1, 2]],
             "answer": True
+        },
+
+        {
+            "input":[3, [0, 1, 1]],
+            "answer": False
+        },
+
+        {
+            "input":[10, [3, 3, 5, 6, 6, 7]],
+            "answer": True
+        },
+
+        {
+            "input":[8, [4, 4, 5, 6, 7]]
+            "answer": True
+        },
+
+        {
+            "input":[7, [4, 4, 5, 6, 7]],
+            "answer": False
+        },
+
+        {
+            "input":[4, [0, 0]],
+            "answer": False
+        },
+
+        {
+            "input":[4, [1, 1]],
+            "answer": True
+        },
+
+        {
+            "input":[4, [2, 2]],
+            "answer": True
+        },
+
+        {
+            "input":[4, [3, 3]],
+            "answer": True
+        },
+
+        {
+            "input":[4, [4, 4]],
+            "answer": False
+        },
+
+        {
+            "input":[4, [0, 0, 0]],
+            "answer": False
+        },
+
+        {
+            "input":[4, [1, 1, 1]],
+            "answer": False
+        },
+
+        {
+            "input":[4, [2, 2, 2]],
+            "answer": False
+        },
+
+        {
+            "input":[4, [3, 3, 3]],
+            "answer": False
+        },
+
+        {
+            "input":[4, [4, 4, 4]],
+            "answer": False
+        },
+
+        {
+            "input":[4, [1, 1, 2, 2]],
+            "answer": False
+        },
+
+        {
+            "input":[4, [2, 2, 3, 3]],
+            "answer": False
+        },
+
+        {
+            "input":[4, [0, 1, 2, 3, 3]],
+            "answer": False
+        },
+
+        {
+            "input":[4, [1, 1, 2, 3, 4]],
+            "answer": False
+        },
+
+        {
+            "input":[4, [0, 1, 2, 3, 4]],
+            "answer": False
         }
 
     ]
